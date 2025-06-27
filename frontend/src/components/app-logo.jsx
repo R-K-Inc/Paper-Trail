e0aa451a2cbc1db5451b83ef3a5df8fb665fb2a2
Kofi Boateng <Kofi.Boatengx@gmail.com>
2025-06-27 18:43:24 -0400

    fixing mssing logo on deployment

--- frontend/src/components/app-logo.jsx
+++ frontend/src/components/app-logo.jsx
@@ -4,7 +4,7 @@ export function AppLogo() {
     return (
         <div className='flex items-center gap-2 h-14'>
             <img
-                src={baseUrl + '/logo.PNG'}
+                src={'/logo.PNG'}
                 alt={appConfig.name}
                 className='h-full w-auto object-contain'
             />
